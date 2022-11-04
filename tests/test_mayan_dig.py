9eb6fc309a15d96f221e9d97b5da29a5201e615e
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-11-05 00:16:05 +0100

    ci: fix broken builds (#2)
    - remove 3.6, 3.7, 3.8
    - add 3.10 and 3.11
    - remove unused setup.py command
    - remove check-manifest
    - ignore base Python conflict
    - fix InterpreterNotFound
    - missing vars in tox.ini
    - change minimum Python version
    - adjust CodeCov config

--- tests/test_mayan_dig.py
+++ tests/test_mayan_dig.py
@@ -1,11 +1,11 @@
-from click.testing import CliRunner
+from typer.testing import CliRunner
 
-from mayan_dig.cli import cabinets
+from mayan_dig.cli import app
 
 
 def test_main():
     runner = CliRunner()
-    result = runner.invoke(cabinets, [])
+    result = runner.invoke(app, ["--help"])
 
-    assert result.output == "()\n"
+    assert "Usage: cabinets [OPTIONS]\n" in result.output
     assert result.exit_code == 0
