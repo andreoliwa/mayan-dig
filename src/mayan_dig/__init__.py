742b42f9a5436224c71ac9b0f73bdd76cc142ffb
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-11-05 05:11:12 +0100

    feat: display documents and metadata

--- src/mayan_dig/__init__.py
+++ src/mayan_dig/__init__.py
@@ -1,1 +1,60 @@
 __version__ = "0.0.0"
+
+import os
+from dataclasses import dataclass
+
+import requests
+import rich
+
+session = requests.Session()
+session.auth = os.environ["MAYAN_ADMIN_USER"], os.environ["MAYAN_ADMIN_PASSWORD"]
+session.headers.update({"Accept": "application/json"})
+
+MAYAN_URL = os.environ["MAYAN_URL"]
+
+
+@dataclass
+class Document:
+    id: int
+    doc_type: str
+    download_url: str
+    label: str
+    filename: str
+    created_at: str
+    description: str
+
+    @classmethod
+    def from_json(cls, data: dict):
+        label = data["label"]
+        filename = data["file_latest"]["filename"]
+        if label == filename:
+            filename = ""
+        return cls(
+            id=data["id"],
+            doc_type=data["document_type"]["label"],
+            download_url=data["file_latest"]["download_url"],
+            label=label,
+            filename=filename,
+            created_at=data["datetime_created"],
+            description=data["description"],
+        )
+
+    @property
+    def label_or_filename(self) -> str:
+        return self.label or self.filename
+
+
+def mayan_url_from_path(url_path: str) -> str:
+    return f"{MAYAN_URL}/api/v4/{url_path}/"
+
+
+def fetch_items_from(url: str, verbose=False):
+    while True:
+        if verbose:
+            rich.print(f"[yellow]Fetching items from[/yellow] [blue]{url}[/blue]")
+        response = session.get(url)
+        data = response.json()
+        yield from data["results"]
+        url = data["next"]
+        if not url:
+            break
--- src/mayan_dig/cli.py
+++ src/mayan_dig/cli.py
@@ -14,53 +14,81 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
-import os
+from pathlib import Path
 from typing import Optional
 
-import requests
+import rich
 import typer
-from rich import print
+
+from mayan_dig import Document
+from mayan_dig import fetch_items_from
+from mayan_dig import mayan_url_from_path
 
 app = typer.Typer()
-
-session = requests.Session()
-session.auth = os.environ["MAYAN_ADMIN_USER"], os.environ["MAYAN_ADMIN_PASSWORD"]
-session.headers.update({"Accept": "application/json"})
-
-MAYAN_URL = os.environ["MAYAN_URL"]
 
 
 @app.command()
 def cabinets(
     full_paths: Optional[list[str]] = typer.Argument(None, help="Partial path name to search"),
-    verbose: bool = typer.Option(False, "--verbose", "-v", help="Verbose"),
+    download_dir: Path = typer.Option(
+        None,
+        "--download-dir",
+        "-d",
+        help="Download directory for the documents; if none, no documents will be downloaded",
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        writable=True,
+        resolve_path=True,
+    ),
+    verbose: int = typer.Option(0, "--verbose", "-v", count=True),
 ):
-    url = f"{MAYAN_URL}/api/v4/cabinets/"
-    if verbose:
-        print(f"[blue]Fetching all cabinets from[/blue] [yellow]{url}[/yellow]")
+    if download_dir:
+        rich.print(f"[yellow]Documents will be downloaded in[/yellow] [blue]{download_dir}[/blue]")
+    else:
+        rich.print("[yellow]No documents will be downloaded[/yellow]")
 
-    selected = []
-    while True:
-        response = session.get(url)
-        data = response.json()
-        url = data["next"]
-        if not url:
-            break
+    selected_cabinets = []
+    for cabinet_dict in fetch_items_from(mayan_url_from_path("cabinets"), verbose):
+        # We don't need the hierarchy for now
+        cabinet_dict.pop("children")
 
-        results = data["results"]
-        for obj in results:
-            # We don't need the hierarchy for now
-            obj.pop("children")
+        if not full_paths:
+            selected_cabinets.append(cabinet_dict)
+            continue
 
-            if not full_paths:
-                selected.append(obj)
+        for path in full_paths:
+            if path.lower() in cabinet_dict["full_path"].lower():
+                selected_cabinets.append(cabinet_dict)
                 continue
 
-            for path in full_paths:
-                if path.lower() in obj["full_path"].lower():
-                    selected.append(obj)
-
-    for obj in selected:
-        print(f"[green]Cabinet:[/green] {obj['full_path']}")
+    for cabinet_dict in selected_cabinets:
+        rich.print(f"[green]Cabinet:[/green] {cabinet_dict['full_path']}")
         if verbose:
-            print(obj)
+            rich.print(cabinet_dict)
+
+        if not download_dir:
+            continue
+
+        for document_dict in fetch_items_from(cabinet_dict["documents_url"], verbose):
+            document = Document.from_json(document_dict)
+            meta = set()
+            for meta_dict in fetch_items_from(mayan_url_from_path(f"documents/{document.id}/metadata"), verbose):
+                meta_dict.pop("document")
+                key = meta_dict["metadata_type"]["label"]
+                value = meta_dict["value"]
+                meta.add(f"{key}-{value}")
+                if verbose >= 2:
+                    rich.print(meta_dict)
+
+            rich_meta = f" [magenta]Metadata:[/magenta] {' '.join(sorted(meta))}" if meta else ""
+            rich.print(
+                f"  [cyan]Document:[/cyan] [magenta]Type:[/magenta] {document.doc_type}"
+                f" [magenta]Name:[/magenta] {document.label_or_filename}"
+                f" [magenta]Created:[/magenta] {document.created_at}{rich_meta}"
+            )
+            if verbose:
+                rich.print(document)
+            if verbose >= 2:
+                rich.print(document_dict)
+            # FIXME: actually download the document and save the file
