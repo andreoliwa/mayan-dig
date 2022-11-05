3bed322ca3b4f9aecbc1d0e03b6aca017d9613b8
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-11-05 11:54:36 +0100

    test: clean output without markup

--- tests/test_mayan_dig.py
+++ tests/test_mayan_dig.py
@@ -2,10 +2,17 @@ from typer.testing import CliRunner
 
 from mayan_dig.cli import app
 
+# https://typer.tiangolo.com/tutorial/commands/help/#rich-markdown-and-markup
+app.rich_markup_mode = None
+
 
 def test_main():
     runner = CliRunner()
     result = runner.invoke(app, ["--help"])
 
-    assert "cabinets [OPTIONS] [FULL_PATHS]..." in result.output
+    for line in [
+        "Usage: cabinets [OPTIONS] [FULL_PATHS]...",
+        "full_paths      [FULL_PATHS]...  Partial path name to search",
+    ]:
+        assert line in result.output
     assert result.exit_code == 0
